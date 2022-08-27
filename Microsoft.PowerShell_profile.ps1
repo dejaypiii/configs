edc0e6cc896f67a4704a7ef385183a06435850c8
dejaypiii <dejaypiii@gmail.com>
2022-08-27 11:41:18 +0200

    Update to binary oh-my-posh

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme agnoster
+function Set-DjpGitPatch { git add --patch }
+Set-Alias -Name gap -Value Set-DjpGitPatch
+
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
