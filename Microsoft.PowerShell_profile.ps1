29aac28d4b7bb890e61eab64a0bf3cc17754ddd3
dejaypiii <dejaypiii@gmail.com>
2022-08-27 11:17:57 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,3 @@
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme Paradox
-
-# PowerShell parameter completion shim for the dotnet CLI 
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-    param($commandName, $wordToComplete, $cursorPosition)
-        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
-}
+Set-PoshPrompt -Theme agnoster
