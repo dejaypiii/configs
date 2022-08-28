466da96464b58f41340d518cadbf010a74833e5e
dejaypiii <dejaypiii@gmail.com>
2022-08-28 21:21:21 +0200

    Update Microsoft.PowerShell_profile.ps1

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,4 @@
 function Set-DjpGitPatch { git add --patch }
 Set-Alias -Name gap -Value Set-DjpGitPatch
 
-oh-my-posh init pwsh | Invoke-Expression
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression
