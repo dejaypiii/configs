bbebb5a58c033367fbbb2a6eca33658bdac960bb
dejaypiii <dejaypiii@gmail.com>
2022-09-10 11:26:27 +0200

    pwsh: Add custom catppuccin-frappe theme

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
 function Set-DjpGitPatch { git add --patch }
 Set-Alias -Name gap -Value Set-DjpGitPatch
 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "~/.catppuccin-frappe-fake.omp.json" | Invoke-Expression
+
