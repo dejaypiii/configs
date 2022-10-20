c3eff4a4b246e2ad773ae4b7d20b6b25ae2fc17c
Johann-Peter Duchon <J.Duchon@hohenstein.com>
2022-10-20 11:13:54 +0200

    nvim: Update deprecated config method

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -36,7 +36,7 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 require('lspconfig')['clangd'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
