cc8ef36d497ea2caebc3f11bbbb82238816b1222
dejaypiii <dejaypiii@gmail.com>
2022-11-30 19:02:58 +0100

    Update powershell profile

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 function Set-DjpGitPatch { git add --patch }
 Set-Alias -Name gap -Value Set-DjpGitPatch
 
+# currently not using ohmyposh
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression
-oh-my-posh init pwsh --config "~/.catppuccin-frappe-fake.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "~/.catppuccin-frappe-fake.omp.json" | Invoke-Expression
 
