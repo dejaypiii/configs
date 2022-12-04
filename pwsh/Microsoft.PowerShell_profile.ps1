a128f544b16a64978b78e469cb22ed13f45c4c70
dejaypiii <dejaypiii@gmail.com>
2022-12-04 10:10:17 +0100

    Simplify vsvim config

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -4,4 +4,5 @@ Set-Alias -Name gap -Value Set-DjpGitPatch
 # currently not using ohmyposh
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression
 # oh-my-posh init pwsh --config "~/.catppuccin-frappe-fake.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\agnoster.minimal.omp.json" | Invoke-Expression
 
